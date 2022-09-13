766486ebdc82fbffbdcc7a65f49a6adec542877b
Alejandro Morales <alejandromorjim@gmail.com>
2022-09-13 10:21:52 -0500

    I took off virtual from de table orders

--- db/migrations/20220908182017-create-orders-TABLE.js
+++ db/migrations/20220908182017-create-orders-TABLE.js
@@ -7,42 +7,29 @@ module.exports = {
   async up (queryInterface) {
     await queryInterface.createTable( ORDER_TABLE , 
       {
-        
         id:{
-              allowNull:false, //no puede ser falso
-              autoIncrement: true,
-              primaryKey: true,
-              type: DataTypes.INTEGER
-          },
-          createdAt:{
-              allowNull: false,
-              type: DataTypes.DATE,
-              field: 'created_at',
-              defaultValue: Sequelize.NOW,
-          },
-          costumerId:{
-              field:"costumer_id",
-              allowNull: false,
-              type: DataTypes.INTEGER,
-              references:{
-                  model: COSTUMER_TABLE, //importa tu modelo User
-                  key: 'id',
-              },
-              onUpdate: 'CASCADE', // Esto ocurre al actualizar, un efecto en cascada y tambien se actualiza
-              onDelete: 'SET NULL' // Esto ocurre al borrar, se establece a null
-          },
-          total:{
-              type: DataTypes.VIRTUAL,
-              get(){
-                  if( this.items.length > 0){
-                      return this.items.reduce((total, item)=>{
-                          return total + ( item.price * item.OrderProduct.amount)
-                      },0)
-                  }
-                  return 0;
-              }
-          }
-      
+            allowNull:false, //no puede ser falso
+            autoIncrement: true,
+            primaryKey: true,
+            type: DataTypes.INTEGER
+        },
+        createdAt:{
+            allowNull: false,
+            type: DataTypes.DATE,
+            field: 'created_at',
+            defaultValue: Sequelize.NOW,
+        },
+        costumerId:{
+            field:"costumer_id",
+            allowNull: false,
+            type: DataTypes.INTEGER,
+            references:{
+                model: COSTUMER_TABLE, //importa tu modelo User
+                key: 'id',
+            },
+            onUpdate: 'CASCADE', // Esto ocurre al actualizar, un efecto en cascada y tambien se actualiza
+            onDelete: 'SET NULL' // Esto ocurre al borrar, se establece a null
+        }
       }
 
     );
